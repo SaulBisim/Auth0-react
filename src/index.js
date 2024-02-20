4e2a8f9fb6f66b18c824bcb1aab32acfec0a2ef7
Saul Rubio <saul.rubio@bi-sim.com.mx>
2024-02-20 16:42:27 -0600

    commit INICIAL

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Auth0Provider} from '@auth0-react';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain='dev-pu34vlwchpjz7rnh.us.auth0.com'
-      clientid='SZLL7NIVkCROufEwhafkfYF4ycdfFbaD'
-      redirectUrl={Window.location.origin}>
+      domain="dev-pu34vlwchpjz7rnh.us.auth0.com"
+      clientId="cZET3II50ROQb264CEPgX72Qjk3BfLcZ"
+      redirectUri={window.location.origin}
+    >
       <App />
     </Auth0Provider>
-    
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
