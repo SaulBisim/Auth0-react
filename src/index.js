67800daca8b5ff84bf89a72fc563a86d8d162024
Saul Rubio <saul.rubio@bi-sim.com.mx>
2024-02-20 16:36:58 -0600

    commit inicial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Auth0Provider} from '@auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-pu34vlwchpjz7rnh.us.auth0.com'
+      clientid='SZLL7NIVkCROufEwhafkfYF4ycdfFbaD'
+      redirectUrl={Window.location.origin}>
+      <App />
+    </Auth0Provider>
+    
   </React.StrictMode>
 );
 
